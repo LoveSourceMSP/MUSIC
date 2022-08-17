d9908d4d1d369eecfa648dc8fc81e9a6749857ef
LoveSourceMSP <108196287+LoveSourceMSP@users.noreply.github.com>
2022-08-17 22:53:45 +0100

    Update running.lua

--- running.lua
+++ running.lua
@@ -7,10 +7,5 @@ shared.delay = nil -- delay overides the ftime
 shared.tempo = nil -- tempo overides the delay
 
 shared.scr = [[
-[8yos] [yos] 8 % [yos] % [4yis] [yis] [4yis] [yis] q [yis] q [yis]
-[8yos] [yos] 8 % [yos] % [4yis] [yis] [4yis] [yis] q [yis] q [yis]
-[9yos] [yos] [yos] [yos][5yos] [yos] [5yoa] [yoa]
-[8yos] [yos] 8 % [yos] % [4yis] [yis] [4yis] [yis] q [yis] q [yis]
-[8yos] [yos] 8 % [yos] % [4yis] [yis] [4yis] [yis] q [yis] q [yis]
-[9yos] [yos] [yos] [yos][5yos] [yos] [5yoa] [yoa] D [8yos] ss [yos] 8% [yos] % [4yis] [yis] 4 [qyid] s [qyid] s [8yod] ss
+ j G a [wry] [wry] j G a [eTu] [eTu] j G a [ryI] [ryI] p a d [ryI] [ryI] j a [wry] [wry] j S [eTu] [eTu] G [ryI] f d [ryI] G G G G j [wry] G a d [eTu] S p [ryI] G G G [eTI] j G G a [wry] a d [eTu] S p [ryI] G G G j G [eTI] j j j G j [wry] G a d [eTu]  S p [ryI] G G h [eTI] j h G f [wry] d d d [eTu] f [wry] d j G [QeyG] d f [wru] [wry] d j G [QeyG] d f [wru] f d d [wru] j j j j [wru] G d d d [eTu] j j [wru] S d d d [ryI] j j j j [ryI] G j [wrI] j a [wrI] G j [wrI] a [wrI] G [wrI] d j j j [wrI] j G [wry] [wry] d j j j [wry] j S [eTu] d [eTu] j j j [eTu] j G [ryI] j [ryI] [ryI] [ryI [ryI] d j j j [ryI] j [ryI] G [ryI] j [ryI] G a [eTu] [eTu] j [eTu] G a [ryI] [ryI] p [ryI] a d [ryI] [ryI] [ryI] j a [wry] [wry [wryj] S [eTu] [eTu] [eTu] G [ryI] f d a
 ]]
