d80a9f7158d34ea4ed7b54fef40debe57557c529
LoveSourceMSP <108196287+LoveSourceMSP@users.noreply.github.com>
2022-08-17 23:01:41 +0100

    Update running.lua

--- running.lua
+++ running.lua
@@ -7,5 +7,144 @@ shared.delay = nil -- delay overides the ftime
 shared.tempo = nil -- tempo overides the delay
 
 shared.scr = [[
-[8f] [9d] [0a] [8f] [9d] [0a] [8f] [9d] [0a] [wa] [9d] [0u] - [8f] [9d] [0a] p o [8f] [9d] [0a] [8f] [9d] [0a] p o p [wa] [0d] [0u] - [8f] [9d] [0a] p o [8f] [9d] [0s] a [8f] [9d] [0a] p o p [wa] [9d] [0u] - a o 8 9 0 p o [8p] o 9 0 u u [8a] a a [9p] p p o 0 u u o o p [wa] [9d] [0a] - a o 8 9 0 p o [8p] o 9 0 u u [8a] a a [9p] p p o 0 u u o o p [wa] [9d] [0a] - p [wa] [9d] [0f]
-]]
+[(w] [(w] [(w] [(w]     [qe] [qe]    [wE] [wE] [wE]    [tY] [tY] [tY] [tY]   [Ey] [Ey]   [wE] [wE] 
+
+[(w] [(w] [(w] [(w]     [qe] [qe]    [wE] [wE] [wE]    [tY] [tY] [tY] [tY]   [Ey] [Ey]   [wE] [wE] 
+
+
+
+[(w] P d g g d d   [qes]  P s   [wEd] s P      [tY] d d  D d s P P  [Ey] [wEg] d P P P 
+
+[(w] g g g  d d      [qes]  P s   [wEd] s P      [tY] d d  D d s P P  [Ey] [wE]
+
+
+
+P P [(wP]  g g P   [qes] g  [wEd]  P   [tYg] g  D D  d d  s  P P  [Ey]  [wE] 
+
+P [(wJ]  j g  P  [qes]  g  [wEd]  P   [tYg] g  D D  d d  s  P P [Ey] [wE] 
+
+
+
+P [(wg] g  D D  d d  s [qes] s [wEd]  s P  [tYo] [Ey] [wE] 
+
+P [(wg] g  D D  d d  s [qes] s [wEd] d d  [tYD] d P   [Ey] [wE] 
+
+
+
+P P [(wp] P [(w] P [(w] P [(ws] P     [qep] P [qeP] P    [wE] P [wEs] P   
+
+ [tYp] P [tYP] P   [Ey] i [Eyi]    g [wE] D [wEd] D [wEd] P [wEs]  
+
+
+[(ws] d [(w] P [(wP] P [(ws] P  [qep] P [qeP] P    [wE] P [wEs] P   
+
+ [tYp] P [tYP] P   [Ey] i [Eyi]    g [wE] D [wEd] D [wEd] P [wEs]  
+
+
+
+o P d d P p P i i i   [tYd]  P  [Eyi]  P  i    [etp] p   [wEP]  i 
+
+[(wo] P d d P   [qeg]  D   [wEd] s   P [tY] s s [Eys] s d  J J  [etg]     
+
+[wEs] s d s P P 
+
+
+
+[(w] s P P   s d s  [qe]   [wEs] s d s P P    [tY] s P P   s d s [Ey] [wEs] s d s P P
+
+[(w] s P P   s d s  [qe]   [wEs] s d s P P    [tY] s P P   s d s [Ey] [wEs] s d s P P
+
+
+
+[(wE]  [(wE]  [(wE]  [(wE]    [qet] [qetP] s d [wEyd] s  [wEyP]  P   [tYoP] s [tYo]  [tYo]  [tYo] 
+
+[Eyi] [EyiP] s d  [wEyd] s [wEyP] P    [(wEP] s [(wE]   [(wE]  [(wE]   
+
+ [qet] [qetP] s d [wEyd] s  [wEyP]  P   [tYoP] s [tYo]  [tYo]  [tYo]  [Eyi] [EyiP] s d  [wEyd] s [wEyP]  P
+
+
+
+
+[(w] P d g g d d   [qes]  P s   [wEd] s P      [tY] d d  D d s P P  [Ey] [wEg] d P P P 
+
+[(w] g g g  d d      [qes]  P s   [wEd] s P      [tY] d d  D d s P P  [Ey] [wE]
+
+
+
+P P [(wP]  g g P   [qes] g  [wEd]  P   [tYg] g  D D  d d  s  P P  [Ey]  [wE] 
+
+P [(wJ]  j g  P  [qes]  g  [wEd]  P   [tYg] g  D D  d d  s  P P [Ey] [wE] 
+
+
+
+P [(wg] g  D D  d d  s [qes] s [wEd]  s P  [tYo] [Ey] [wE] 
+
+P [(wg] g  D D  d d  s [qes] s [wEd] d d  [tYD] d P   [Ey] [wE] 
+
+
+
+P P [(wp] P [(w] P [(w] P [(ws] P     [qep] P [qeP] P    [wE] P [wEs] P   
+
+ [tYp] P [tYP] P   [Ey] i [Eyi]    g [wE] D [wEd] D [wEd] P [wEs]  
+
+
+[(ws] d [(w] P [(wP] P [(ws] P  [qep] P [qeP] P    [wE] P [wEs] P   
+
+ [tYp] P [tYP] P   [Ey] i [Eyi]    g [wE] D [wEd] D [wEd] P [wEs]  
+
+
+
+o P d d P p P i i i   [tYd]  P  [Eyi]  P  i    [etp] p   [wEP]  i 
+
+[(wo] P d d P   [qeg]  D   [wEd] s   P [tY] s s [Eys] s d  J J  [etg]     
+
+[wEs] s d s P P 
+
+
+
+[(w] s P P   s d s  [qe]   [wEs] s d s P P    [tY] s P P   s d s [Ey] [wEs] s d s P 
+
+
+
+[(g]   w  E  [Yd]  o     [qet]   [wEyd]  s P s P    [tYos]  d d   [Eyi]   [wEyd]  s P s P 
+
+[(s]  w   [Ed]  [Yd] o   P P   g [qet] D d   [wEyD]  d  P s    [tYos] d   [Eyi] [wEy] 
+
+
+P  [(g]   w  E  [Yd]  o     [qet]   [wEyd]  s P s P    [tYos]  d d   [Eyi]   [wEy]
+
+g D d D  [(D]  w  [EP]  [Ys]  o    P P   g [qet] D d   [wEyD]  d  P s    [tYos] d   [Eyi] [wEy] 
+
+
+
+
+P P [(wp] P [(w] P [(w] P [(ws] P     [qep] P [qeP] P    [wE] P [wEs] P   
+
+ [tYp] P [tYP] P   [Ey] i [Eyi]    g [wE] D [wEd] D [wEd] P [wEs]  
+
+
+[(ws] d [(w] P [(wP] P [(ws] P  [qep] P [qeP] P    [wE] P [wEs] P   
+
+ [tYp] P [tYP] P   [Ey] i [Eyi]    g [wE] D [wEd] D [wEd] P [wEs]  
+d
+
+
+o P d d P p P i i i   [tYd]  P  [Eyi]  P  i    [etp] p   [wEP]  i 
+
+[(wo] P d d P   [qeg]  D   [wEd] s   P [tY] s s [Eys] s d  J J  [etg]     
+tuo
+[wEs] s d s P P 
+
+
+
+[(w] s P P   s d s  [qe]   [wEs] s d s P P    [tY] s P P   s d s [Ey] [wEs] s d s P P
+
+
+
+(   w   [Es]   [YP] P o     s P P    [qet]  s  [wEyP] P   s P P   [tYo] s P P
+
+s P P [Eyi]   s [wEyP] P  s P P 
+
+
+
+( w E Y o       [qet]    [wEy]     [tYo]  o Y t Y  [Eyi]  [wEy]  Y y t]]
