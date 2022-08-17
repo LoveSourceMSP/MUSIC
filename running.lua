ca6763bd932e47060b142cf8ac0c5ef87882fbec
LoveSourceMSP <108196287+LoveSourceMSP@users.noreply.github.com>
2022-08-17 17:54:07 +0100

    Update running.lua

--- running.lua
+++ running.lua
@@ -1,3 +1,7 @@
+-- Sheet for Rondo Alla Turca by Wolfgang Amadeus Mozart
+
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
 shared.ftime = 9*60 + 25 -- time in seconds for the song to finish (extended by |)
 shared.delay = nil -- delay overides the ftime
 shared.tempo = nil -- tempo overides the delay
