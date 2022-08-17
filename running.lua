c3d177148796313a8cf08cc0c714f3da77833003
LoveSourceMSP <108196287+LoveSourceMSP@users.noreply.github.com>
2022-08-17 23:10:27 +0100

    Update running.lua

--- running.lua
+++ running.lua
@@ -7,5 +7,35 @@ shared.delay = nil -- delay overides the ftime
 shared.tempo = nil -- tempo overides the delay
 
 shared.scr = [[
-w y o P d tf | w y o P d tf | d P s d s P s d P o dddd sss P d P o oo dddd sss P d P o oo dddd sss P d P s s sss s sss d s ppp d s ppp p P p o | ddd d s oo i o P o i o | s ddd d s i o P o i o | d d d P sss P s d D d PsPsPsPsPs s P s P o i o P i o | d P s d s P s d P o dddd sss P d P o oo dddd sss P d P o oo dddd sss P d P s s sss s sss d s ppp d s pppp p PpP d f | o P d f | P p s P s p P o P p s P s p P o | P dddd PP s P s PpPpPp o dd d | h g d D s p pPp o | d P s d | s P s d P o dddd sss P d P o oo dddd sss P o oo dddd sss P d P s s sss s sss d s ppp ds pppp pPpP d f | o P d s | d h d D d PP f P d h h J h J h d h d DDD d DD g D d h h J h J h h l J hhh l h j j jJ hgh h h g J hgh | o P d f | o P d f | o P d f | o P d f | P s d | s P s d P o dddd sss P d P o oo dddd sss P d P o oo dddd sss P d P s s sss s sss d s ppp d s pppp p P p o |
+ t t T T T T T T t t T T T T T T D D-D-D-D S a O-O D S a O-O-I S S-D-a P P O-O-O
+D S a O-O D S a O-O-I
+S S-D-a P P
+W-W-W-T Y-Y T-Y
+r-T-T r T
+r-T-T r T T r T r T-Y T r W
+T Y-Y T-Y
+r-T-T r T
+r-T-T r T T r T r T-Y T r W
+Y-Y-T-T-Y O Y T
+E-W E-W E W
+T r E W
+Y-Y-T-T-Y O Y T
+E-W E-W E W
+T r E W
+T Y-Y T-Y W
+r-T-T r T
+r-T-T r T W T r T r T-Y T r W
+T Y-Y T-Y W
+r-T-T r T
+T Y-Y T-Y W
+T r T-Y T r W
+ Y-Y-T-T-Y O Y T
+E-W E-W E W
+T r E W
+O-O-O D S a O-O D S a O-O-I
+S S-D-a P P O-O-O
+D S a O-O D S S O-O-I
+S S-D-a P O O-O-O-O
+ t t T T T T T T
+ t t T T T T T T
 ]]
